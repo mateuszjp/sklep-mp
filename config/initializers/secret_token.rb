e0ee8046cb221f3baa93fbc8f7479f6e21db77a9
mateuszjp <mateuszjp@gmail.com>
2013-10-20 15:14:38 +0200

    first commit setup db,gemfile for dev and production

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sklep::Application.config.secret_key_base = 'f500f7218d334589c1b90fe063c06fcb96753db73bddf1982c40d9d66483b73501bfffca94970e2255c83e6db1bb98e26e44119d031e8c08c395748b867ff134'
